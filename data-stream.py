2a512da5266e8494e2754630f72c6e2e0e9bf0b1
Noble <guolei1.chen@gmail.com>
2018-11-12 17:43:43 -0500

    update data-stream

--- data-stream.py
+++ data-stream.py
@@ -1,7 +1,8 @@
 import argparse
 import atexit
-import json
 import logging
+import json
+import time
 
 from kafka import KafkaProducer
 from kafka.errors import KafkaError, KafkaTimeoutError
@@ -13,4 +14,51 @@ logger_format = '%(asctime)s - %(message)s'
 logging.basicConfig(format=logger_format)
 logger = logging.getLogger('stream-processing')
 logger.setLevel(logging.INFO)
+
+
+def shutdown_hook(producer):
+    """
+    a shutdown hook to be called before the shutdown
+    """
+
+
+def process_stream(stream, kafka_producer, target_topic):
+
+
+if __name__ == '__main__':
+	# Setup command line arguments.
+	parser = argparse.ArgumentParser()
+	parser.add_argument('source_topic')
+	parser.add_argument('target_topic')
+	parser.add_argument('kafka_broker')
+	parser.add_argument('batch_duration', help='the batch duration in secs')
+
+	# Parse arguments.
+	args = parser.parse_args()
+	source_topic = args.source_topic
+	target_topic = args.target_topic
+	kafka_broker = args.kafka_broker
+	batch_duration = int(args.batch_duration)
+
+	# Create SparkContext and StreamingContext.
+	sc = SparkContext("local[2]", "AveragePrice")
+	sc.setLogLevel('INFO')
+	ssc = StreamingContext(sc, batch_duration)
+
+	# Instantiate a kafka stream for processing.
+	directKafkaStream = KafkaUtils.createDirectStream(ssc, [source_topic], { 'metadata.broker.list':kafka_broker })
+
+	# Extract value from directKafkaStream (key, value) pair.
+	stream = directKafkaStream.map(lambda msg:msg[1])
+
+	# Instantiate a simple kafka producer.
+	kafka_producer = KafkaProducer(bootstrap_servers=kafka_broker)
+
+	process_stream(stream, kafka_producer, target_topic)
+
+	# Setup shutdown hook.
+	atexit.register(shutdown_hook, kafka_producer)
+
+	ssc.start()
+	ssc.awaitTermination()
 
