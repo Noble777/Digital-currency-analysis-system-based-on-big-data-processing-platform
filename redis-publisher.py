9fe39cd1420f0dba7cc3ba9e0f05e2935ac8b951
Noble <guolei1.chen@gmail.com>
2018-11-15 17:18:15 -0500

    update redis-publisher

--- redis-publisher.py
+++ redis-publisher.py
@@ -8,5 +8,15 @@ from kafka import KafkaConsumer
 logger_format = '%(asctime)s - %(message)s'
 logging.basicConfig(format=logger_format)
 logger = logging.getLogger('redis-publisher')
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
+
+
+def shutdown_hook(kafka_consumer):
+	"""
+	A shutdown hook to be called before the shutdown.
+	"""
+	try:
+		kafka_consumer.close(10)
+	except Exception as e:
+		logger.warn('Failed to close kafka consumer for %s', e)
 
